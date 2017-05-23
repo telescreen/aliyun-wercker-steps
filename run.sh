8528c4b92cf7a12496dcedbe26a4eafc0e0b8427
telescreen <telescreen@macbook-air>
2017-05-23 15:04:34 +0900

    update

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ pip install aliyun-python-sdk-slb aliyun-python-sdk-ecs
 pip install aliyun-python-sdk-rds aliyun-python-sdk-oss aliyun-python-sdk-r-kvstore
 pip install aliyun-python-sdk-sts aliyun-python-sdk-ess
 
+echo "aliyuncli ecs CreateImage --AccessKeyId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_ID --AccessKeySecret $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_SECRET --RegionId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_REGION_ID --InstanceId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_INSTANCE_ID --ImageName $WERCKER_ALIYUN_BUILD_ECS_IMAGE_IMAGE_NAME"
+
 aliyuncli ecs CreateImage --AccessKeyId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_ID --AccessKeySecret $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_SECRET --RegionId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_REGION_ID --InstanceId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_INSTANCE_ID --ImageName $WERCKER_ALIYUN_BUILD_ECS_IMAGE_IMAGE_NAME
