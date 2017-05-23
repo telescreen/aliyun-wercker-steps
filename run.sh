05381364e45eaed69a73e9624710d94fc5ac869d
tal <tal@tals-MacBook-Air.local>
2017-05-23 14:16:54 +0900

    Update aliyuncli

--- run.sh
+++ run.sh
@@ -6,19 +6,4 @@ pip install aliyun-python-sdk-slb aliyun-python-sdk-ecs
 pip install aliyun-python-sdk-rds aliyun-python-sdk-oss aliyun-python-sdk-r-kvstore
 pip install aliyun-python-sdk-sts aliyun-python-sdk-ess
 
-echo <<< EOF
-[default]
-aliyun_access_key_secret = $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_ID
-aliyun_access_key_id = $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_SECRET
-
-EOF >>  ~/.aliyuncli/credentials;
-
-echo <<< EOF
-
-[default]
-region = $WERCKER_ALIYUN_BUILD_ECS_IMAGE_REGION_ID
-output = json
-
-EOF
-
-aliyuncli ecs CreateImage --InstanceId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_INSTANCE_ID_ --ImageName $WERCKER_ALIYUN_BUILD_ECS_IMAGE_IMAGE_NAME
+aliyuncli ecs CreateImage --AccessKeyId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_ID --AccessKeySecret $WERCKER_ALIYUN_BUILD_ECS_IMAGE_ACCESS_KEY_SECRET --RegionId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_REGION_ID --InstanceId $WERCKER_ALIYUN_BUILD_ECS_IMAGE_INSTANCE_ID --ImageName $WERCKER_ALIYUN_BUILD_ECS_IMAGE_IMAGE_NAME
